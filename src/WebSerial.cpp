2c329a5eb2c63b846c140ea5e33e4b55d4aa1dcb
Mathieu Carbou <mathieu.carbou@gmail.com>
2024-05-31 16:14:45 +0200

    Added WebSerialLite: a lightweight high-performance version of WebSerial - WebSerialLite is able to stream in real-time at a speed of more than 20 messages per second with a low memory footprint - There is no synchronization or mutex in place (be careful when 2 cores are writing) - There is no buffering by default, so write(c), print(c), printf(...) and variants with only 1 character are not allowed - WebSerialLite can use a special line buffering mode to only send complete lines to the browser - suitable for redirecting Arduino logs to the browser

--- src/WebSerial.cpp
+++ src/WebSerial.cpp
@@ -69,6 +69,10 @@ void WebSerialClass::begin(AsyncWebServer *server, const char* url) {
     // if(type == WS_EVT_CONNECT){
     // } else if(type == WS_EVT_DISCONNECT){
     // } else if(type == WS_EVT_DATA){
+    if (type == WS_EVT_CONNECT) {
+      client->setCloseClientOnQueueFull(false);
+      return;
+    }
     if(type == WS_EVT_DATA){
       // Detect magic bytes
       if (data[0] == WSL_MAGIC_BYTE_1 && data[1] == WSL_MAGIC_BYTE_2) {
@@ -113,12 +117,57 @@ void WebSerialClass::onMessage(WSLStringMessageHandler callback) {
 
 // Print func
 size_t WebSerialClass::write(uint8_t m) {
+  if (!_ws)
+    return 0;
+
+#ifdef WSL_HIGH_PERF
+  // We do not support non-buffered write on webserial for the HIGH_PERF version
+  // we fail with a stack trace allowing the user to change the code to use write(const uint8_t* buffer, size_t size) instead
+  if(!_initialBufferCapacity) {
+#ifdef ESP8266
+    ets_printf("Non-buffered write is not supported. Please use write(const uint8_t* buffer, size_t size) instead.");
+#else
+    log_e("Non-buffered write is not supported. Please use write(const uint8_t* buffer, size_t size) instead.");
+#endif
+    assert(false);
+    return 0;
+  }
+#endif // WSL_HIGH_PERF
+
   write(&m, 1);
   return(1);
 }
 
 // Println / Printf / Write func
 size_t WebSerialClass::write(const uint8_t* buffer, size_t size) {
+  if (!_ws || size == 0)
+    return 0;
+
+#ifdef WSL_HIGH_PERF
+  // No buffer, send directly (i.e. use case for log streaming)
+  if (!_initialBufferCapacity) {
+    size = buffer[size - 1] == '\n' ? size - 1 : size;
+    _send(buffer, size);
+    return size;
+  }
+
+  // fill the buffer while sending data for each EOL
+  size_t start = 0, end = 0;
+  while (end < size) {
+    if (buffer[end] == '\n') {
+      if (end > start) {
+        _buffer.concat(reinterpret_cast<const char*>(buffer + start), end - start);
+      }
+      _send(reinterpret_cast<const uint8_t*>(_buffer.c_str()), _buffer.length());
+      start = end + 1;
+    }
+    end++;
+  }
+  if (end > start) {
+    _buffer.concat(reinterpret_cast<const char*>(buffer + start), end - start);
+  }
+  return size;
+#else
   loop();
   _wait_for_print_mutex();
   _print_buffer_mutex = true;
@@ -134,8 +183,33 @@ size_t WebSerialClass::write(const uint8_t* buffer, size_t size) {
   _print_buffer_mutex = false;
   _last_print_buffer_write_time = micros();
   return(size);
+#endif
 }
 
+#ifdef WSL_HIGH_PERF
+void WebSerialClass::_send(const uint8_t* buffer, size_t size) {
+  if (_ws && size > 0) {
+    _ws->cleanupClients(WSL_MAX_WS_CLIENTS);
+    if (_ws->count()) {
+      if (size > UINT16_MAX)
+        size = UINT16_MAX;
+      AsyncWebSocketMessageBuffer* wsbuffer = _ws->makeBuffer(WSL_HEAD_LEN + WSL_MSG_SIZE_LEN + size);
+      _write_row_packet(wsbuffer->get(), buffer, size);
+      _ws->binaryAll(wsbuffer);
+    }
+  }
+
+  // if buffer grew too much, free it, otherwise clear it
+  if (_initialBufferCapacity) {
+    if (_buffer.length() > _initialBufferCapacity) {
+      setBuffer(_initialBufferCapacity);
+    } else {
+      _buffer.clear();
+    }
+  }
+}
+
+#else // WSL_HIGH_PERF
 void WebSerialClass::_wait_for_global_mutex() {
   // Wait for mutex to be released
   if (_buffer_mutex) {
@@ -224,8 +298,10 @@ void WebSerialClass::_flush_global_buffer() {
     _buffer_mutex = false;
   }
 }
+#endif // WSL_HIGH_PERF
 
 void WebSerialClass::loop() {
+#ifndef WSL_HIGH_PERF
   if ((unsigned long)(millis() - _last_cleanup_time) > WSL_CLEANUP_TIME_MS) {
     _last_cleanup_time = millis();
     _ws->cleanupClients(WSL_MAX_WS_CLIENTS);
@@ -244,10 +320,20 @@ void WebSerialClass::loop() {
       _flush_global_buffer();
     }
   }
+#endif // WSL_HIGH_PERF
+}
+
+void WebSerialClass::setBuffer(size_t initialCapacity) {
+#ifdef WSL_HIGH_PERF
+  assert(initialCapacity <= UINT16_MAX);
+  _initialBufferCapacity = initialCapacity;
+  _buffer = String();
+  _buffer.reserve(initialCapacity);
+#endif
 }
 
 size_t WebSerialClass::_write_row_packet(uint8_t* dest, const uint8_t *payload, size_t payload_size) {
-    // sanity check to ensure the payload size is within the hard limit
+  // sanity check to ensure the payload size is within the hard limit
   if(payload_size > UINT16_MAX)
     payload_size = UINT16_MAX;
   // Write header
--- src/WebSerial.h
+++ src/WebSerial.h
@@ -39,42 +39,53 @@ License: AGPL-3.0 (https://www.gnu.org/licenses/agpl-3.0.html)
 #define WSL_MAX_WS_CLIENTS DEFAULT_MAX_WS_CLIENTS
 #endif
 
-// Global buffer ( buffers all packets )
-#ifndef WSL_BUFFER_SIZE
-#define WSL_BUFFER_SIZE                       2048
-#endif
-#ifndef WSL_PRINT_BUFFER_SIZE
-#define WSL_PRINT_BUFFER_SIZE                 1024
-#endif
-#ifndef WSL_MAX_ROW_PACKET_PAYLOAD_SIZE
-#define WSL_MAX_ROW_PACKET_PAYLOAD_SIZE       512
-#endif
+// High performance mode:
+// - Low memory footprint (no stack allocation, no global buffer by default)
+// - Low latency (messages sent immediately to the WebSocket queue)
+// - High throughput (up to 20 messages per second, no locking mechanism)
+// Activation with: -D WSL_HIGH_PERFORMANCE
+// Also recommended to tweak AsyncTCP and ESPAsyncWebServer settings, for example:
+//  -D CONFIG_ASYNC_TCP_QUEUE_SIZE=128  // AsyncTCP queue size
+//  -D CONFIG_ASYNC_TCP_RUNNING_CORE=1  // core for the async_task
+//  -D WS_MAX_QUEUED_MESSAGES=128       // WS message queue size
+#ifndef WSL_HIGH_PERF
+  // Global buffer ( buffers all packets )
+  #ifndef WSL_BUFFER_SIZE
+  #define WSL_BUFFER_SIZE                       2048
+  #endif
+  #ifndef WSL_PRINT_BUFFER_SIZE
+  #define WSL_PRINT_BUFFER_SIZE                 1024
+  #endif
+  #ifndef WSL_MAX_ROW_PACKET_PAYLOAD_SIZE
+  #define WSL_MAX_ROW_PACKET_PAYLOAD_SIZE       512
+  #endif
 
-#ifndef WSL_PRINT_FLUSH_TIME_US
-#define WSL_PRINT_FLUSH_TIME_US               100
-#endif
-#ifndef WSL_GLOBAL_FLUSH_TIME_MS
-#define WSL_GLOBAL_FLUSH_TIME_MS              100
-#endif
-#ifndef WSL_CLEANUP_TIME_MS
-#define WSL_CLEANUP_TIME_MS                   5000
-#endif
+  #ifndef WSL_PRINT_FLUSH_TIME_US
+  #define WSL_PRINT_FLUSH_TIME_US               100
+  #endif
+  #ifndef WSL_GLOBAL_FLUSH_TIME_MS
+  #define WSL_GLOBAL_FLUSH_TIME_MS              100
+  #endif
+  #ifndef WSL_CLEANUP_TIME_MS
+  #define WSL_CLEANUP_TIME_MS                   5000
+  #endif
 
-#if WSL_BUFFER_SIZE < 512
-  #error "WSL_BUFFER_SIZE must be >= 512 bytes"
-#endif
+  #if WSL_BUFFER_SIZE < 512
+    #error "WSL_BUFFER_SIZE must be >= 512 bytes"
+  #endif
 
-#if WSL_BUFFER_SIZE < WSL_PRINT_BUFFER_SIZE
-  #error "WSL_BUFFER_SIZE must be >= WSL_PRINT_BUFFER_SIZE"
-#endif
+  #if WSL_BUFFER_SIZE < WSL_PRINT_BUFFER_SIZE
+    #error "WSL_BUFFER_SIZE must be >= WSL_PRINT_BUFFER_SIZE"
+  #endif
 
-#if WSL_PRINT_FLUSH_TIME_US < 1
-  #error "WSL_PRINT_FLUSH_TIME_US must be greater than 1us"
-#endif
+  #if WSL_PRINT_FLUSH_TIME_US < 1
+    #error "WSL_PRINT_FLUSH_TIME_US must be greater than 1us"
+  #endif
 
-#if WSL_GLOBAL_FLUSH_TIME_MS < 50
-  #error "WSL_GLOBAL_FLUSH_TIME_MS must be greater than 50ms"
-#endif
+  #if WSL_GLOBAL_FLUSH_TIME_MS < 50
+    #error "WSL_GLOBAL_FLUSH_TIME_MS must be greater than 50ms"
+  #endif
+#endif // WSL_HIGH_PERFORMANCE
 
 typedef std::function<void(uint8_t *data, size_t len)> WSLMessageHandler;
 typedef std::function<void(const String& msg)> WSLStringMessageHandler;
@@ -88,9 +99,37 @@ class WebSerialClass : public Print {
     void onMessage(WSLStringMessageHandler recv);
     size_t write(uint8_t) override;
     size_t write(const uint8_t* buffer, size_t size) override;
+    
+    // Only valid if WSL_HIGH_PERF is not activated (which is the default)
+    // Housekeeping for WebSerial internals.
+    // Calling this loop has no effect if WSL_HIGH_PERF is activated
     void loop();
+    
+    // Only valid if WSL_HIGH_PERF is activated
+    // A buffer (shared across cores) can be initialised with an initial capacity to be able to use any Print functions event those that are not buffered and would
+    // create a performance impact for WS calls. The goal of this buffer is to be used with lines ending with '\n', like log messages.
+    // The buffer size will eventually grow until a '\n' is found, then the message will be sent to the WS clients and a new buffer will be created.
+    // Set initialCapacity to 0 to disable buffering.
+    // Must be called before begin(): calling it after will erase the buffer and its content will be lost.
+    // The buffer is not enabled by default.
+    void setBuffer(size_t initialCapacity);
 
   private:
+    // Server
+    AsyncWebServer *_server;
+    AsyncWebSocket *_ws;
+    WSLMessageHandler _recv = nullptr;
+    bool _authenticate = false;
+    String _username;
+    String _password;
+
+#ifdef WSL_HIGH_PERF
+    size_t _initialBufferCapacity = 0;
+    String _buffer;
+    void _send(const uint8_t* buffer, size_t size);
+#else
+    unsigned long _last_cleanup_time = 0;
+
     // Global Buffer
     bool _buffer_mutex = false;
     size_t _buffer_offset = 0;
@@ -103,15 +142,6 @@ class WebSerialClass : public Print {
     unsigned long _last_print_buffer_write_time = 0;
     unsigned long _last_print_buffer_flush_time = 0;
 
-    // Server
-    AsyncWebServer *_server;
-    AsyncWebSocket *_ws;
-    WSLMessageHandler _recv = nullptr;
-    unsigned long _last_cleanup_time = 0;
-    bool _authenticate = false;
-    String _username;
-    String _password;
-
     // Print
     void _wait_for_global_mutex();
     void _wait_for_print_mutex();
@@ -121,6 +151,7 @@ class WebSerialClass : public Print {
     size_t _end_row();
     void _flush_print_buffer();
     void _flush_global_buffer();
+#endif
 
     static size_t _write_row_packet(uint8_t* dest, const uint8_t *payload, size_t payload_size);
 };
